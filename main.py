00335635cc5aa286209f2ff1083aa2f71e89b586
moon <moon0xcoder@gmail.com>
2023-06-11 20:32:02 +0200

    Enhance the system to be more efficient
    Removed additional reference storing in the code to make it more
    efficient. Added min and max temperatures in the script.
    
    Simplified the fan change state method by using the boolean value of the
    condition used for the `fans_output.value()` function.
    
    Made the system fully work by investigating the way the double relay
    module worked. It actives on low pulses rather than on high pulses.
    Thus, inverting the condition of the fan state is needed to achieve the
    desired result.

---
+++ main.py
@@ -0,0 +1,36 @@
+from machine import Pin, Timer, SoftI2C
+from bme280 import BME280
+from ssd1306 import SSD1306_I2C
+
+i2cbus = SoftI2C(scl=Pin(22), sda=Pin(21))
+bme280 = BME280(i2c=i2cbus)
+oled = SSD1306_I2C(128, 64, i2cbus)
+# sct013 = ...
+
+fans_output = Pin(2, Pin.OUT)
+
+# Constants
+
+TEMP_IDX = 0
+PRES_IDX = 1
+HUM_IDX = 2
+MIN_TEMP = 20
+MAX_TEMP = 27
+
+def update_oled():
+    # oled.text(f'TEMP: {int(sct013.values[0])}W', 0, 0)
+    oled.text(f'TEMP: {bme280.values[TEMP_IDX]}C', 0, 17)
+    oled.text(f'PRES: {bme280.values[PRES_IDX]}hPa', 0, 33)
+    oled.text(f'HUM: {bme280.values[HUM_IDX]}%', 0, 49)
+
+    oled.show()
+    oled.fill(0)
+
+update_fans = lambda: fans_output.value(not bme280.values[TEMP_IDX] > MAX_TEMP) # Relay actives on low, therefore, the condition is inverted with 'not'
+
+def listener(t):
+    update_oled()
+    update_fans()
+
+listener_handler = Timer(0)
+listener_handler.init(period=500, callback=listener)
